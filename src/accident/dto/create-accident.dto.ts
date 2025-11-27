abd4e16ef1edf9ca047cb08ec7bd60b9f95ddcdd
jurayevd3v <jurayevd3v@gmail.com>
2025-11-27 11:18:22 +0500

    cahnge

--- src/accident/dto/create-accident.dto.ts
+++ src/accident/dto/create-accident.dto.ts
@@ -1,3 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty } from 'class-validator';
+
 export class CreateAccidentDto {
+  @ApiProperty({ example: true, description: 'Satatus' })
+  @IsNotEmpty()
+  @IsBoolean()
   status: boolean;
 }
--- src/fine/dto/create-fine.dto.ts
+++ src/fine/dto/create-fine.dto.ts
@@ -1,3 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty } from 'class-validator';
+
 export class CreateFineDto {
+  @ApiProperty({ example: true, description: 'Satatus' })
+  @IsNotEmpty()
+  @IsBoolean()
   status: boolean;
 }
